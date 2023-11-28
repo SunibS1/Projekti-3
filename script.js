93f670dba378ef6622d210a6af79a6802b39fa30
SunibS1 <sunib.spahiu2015@gmail.com>
2023-11-28 18:55:53 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,12 @@
+// let rating = document.getElementById("star-rating")
+
+// let review = function() {
+//     if (this.value == 1) {
+//         return "Poor"   
+//     }
+// }
+
+
+function submitReview(rating) {
+    console.log('User rated:', rating);
+}
